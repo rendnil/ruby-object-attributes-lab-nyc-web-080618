7f291f81f01bc4a2ca2c2516dc77c9f3bcced972
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-08-07 14:44:37 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def name
+    "#{@name}"
+  end  
+  
+  
+  def job=(dog_job)
+    @job = dog_job
+  end
+  
+  def job
+    "#{@job}"
+  end  
+  
+end  
